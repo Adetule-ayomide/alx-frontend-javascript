9da2309e3cda90f8326d139532509cf783366938
Adetule-ayomide <adetuleayomide@gmail.com>
2024-03-06 15:39:39 +0000

    promise

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,8 +1,10 @@
 import { uploadPhoto, createUser } from './utils';
-
+// eslint-disable-next-line
 export default function handleProfileSignup() {
+  // eslint-disable-next-line
   Promise.all([uploadPhoto(), createUser()])
     .then((data) => {
+      // eslint-disable-next-line
       const upload = data[0];
       const create = data[1];
 
