8cac4141466b725e87e5ad6a96fc77a69dd0b2c7
Adetule-ayomide <adetuleayomide@gmail.com>
2024-03-06 16:08:39 +0000

    promise

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,5 +1,7 @@
 export default function signUpUser(firstName, lastName) {
+  /* eslint-disable no-unused-vars */
   return new Promise((resolve, reject) => {
-    resolve({firstName, lastName});
+    /* eslint-disable no-unused-vars */
+    resolve({ firstName, lastName });
   });
 }
