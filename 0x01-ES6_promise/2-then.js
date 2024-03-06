3e64214ddba64988f7e5d676c4d868b3faa9bec0
Adetule-ayomide <adetuleayomide@gmail.com>
2024-03-06 13:58:57 +0000

    promise

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,5 +1,7 @@
+// eslint-disable-next-line
 export default function handleResponseFromAPI(promise) {
   return promise
+    // eslint-disable-next-line
     .then(() => ({ status: 200, body: 'Success' }))
     .catch(() => new Error())
     .finally(() => { console.log('Got a response from the API'); });
