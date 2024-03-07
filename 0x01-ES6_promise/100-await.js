cb128122cab6f3c2bc2d082cefb05a5ad4957a58
Adetule-ayomide <adetuleayomide@gmail.com>
2024-03-07 00:45:47 +0000

    promise

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,21 +1,14 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
-  let upload, create;
-
+  let photo;
+  let user;
   try {
-    [upload, create] = await Promise.all([uploadPhoto(), createUser()]);
-    return {
-      photo: upload,
-      user: create,
-    };
+    photo = await uploadPhoto();
+    user = await createUser();
   } catch (error) {
-
-    if (upload && upload.status === "rejected" || create && create.status === "rejected") {
-      return {
-        photo: null,
-        user: null,
-      };
-    }
+    photo = null;
+    user = null;
   }
+  return { photo, user };
 }
