b9f38dce4f6117fb1e1a3c265d51f44ac160019c
Adetule-ayomide <adetuleayomide@gmail.com>
2024-03-07 02:18:33 +0000

    promise

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,12 +4,10 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, fileName) {
   const signUp = signUpUser(firstName, lastName);
   const upload = uploadPhoto(fileName);
-  const user = [];
 
   return Promise.allSettled([signUp, upload])
-    .then((results) => results.map((result) => (user.push({
+    .then((results) => results.map((result) => ({
       status: result.status,
       value: result.status === 'fulfilled' ? result.value : result.reason.message,
-    }))));
-    return user;
+    })));
 }
