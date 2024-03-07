87e79b239795c4ebba68e90e730f198c87ac6c70
Adetule-ayomide <adetuleayomide@gmail.com>
2024-03-07 17:24:25 +0000

    classess

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,9 +1,9 @@
 import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
-  const Class_one = new ClassRoom(19);
-  const Class_two = new ClassRoom(20);
-  const Class_three = new ClassRoom(34);
+  const classOne = new ClassRoom(19);
+  const classTwo = new ClassRoom(20);
+  const classThree = new ClassRoom(34);
 
-  return [Class_one, Class_two, Class_three];
+  return [classOne, classTwo, classThree];
 }
