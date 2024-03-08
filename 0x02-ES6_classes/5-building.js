27c81add808033259a0e81d49522aed236ded8a6
Adetule-ayomide <adetuleayomide@gmail.com>
2024-03-08 00:16:10 +0000

    classes

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -17,16 +17,3 @@ export default class Building {
     this._sqft = sqft;
   }
 }
-
-
-const b = new Building(100);
-console.log(b);
-
-class TestBuilding extends Building {}
-
-try {
-    new TestBuilding(200)
-}
-catch(err) {
-    console.log(err);
-}
