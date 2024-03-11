2c7531022a40dbd4eadf977b397e09d7fc6b8b10
Adetule-ayomide <adetuleayomide@gmail.com>
2024-03-11 21:10:54 +0000

    data manipulation

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,9 +1,9 @@
 export default function getListStudents() {
   const listStudent = [
-    {id: 1, firstName: 'Guillaume', location: 'San Francisco'},
-    {id: 2, firstName: 'James', location: 'Columbia'},
-    {id: 3, firstName: 'Serena', location: 'San Francisco'}
-  ]
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 3, firstName: 'Serena', location: 'San Francisco' },
+  ];
 
   return listStudent;
-};
+}
