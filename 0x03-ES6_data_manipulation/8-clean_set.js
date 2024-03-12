23766083e0108979a62d702f1a09fd41a61f78bb
Adetule-ayomide <adetuleayomide@gmail.com>
2024-03-12 15:18:55 +0000

    data manipulation

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,9 @@
 export default function cleanSet(set, string) {
-  if (string.length === 0) return '';
+  if (!string) return '';
   const setFilter = [...set].filter((item) => item.startsWith(string));
   const setString = setFilter.map((item) => item.slice(string.length)).join('-');
   return setString;
 }
+
+console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), 'bon'));
+console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), ''));
