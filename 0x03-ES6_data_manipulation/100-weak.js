225a0d6b18003e9d1409b75b9c3644bbb3f2536c
Adetule-ayomide <adetuleayomide@gmail.com>
2024-03-12 23:31:30 +0000

    data manipulation

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,8 +1,9 @@
 export const weakMap = new WeakMap();
-export default function queryAPI(endpoint) {
-  const count = weakMap.get(endpoint) || 0;
+export function queryAPI(endpoint) {
+  let count = weakMap.get(endpoint) || 0;
+  count += 1;
   if (count >= 5) {
     throw new Error('Endpoint load is high');
   }
-  weakMap.set(endpoint, count + 1);
+  weakMap.set(endpoint, count);
 }
